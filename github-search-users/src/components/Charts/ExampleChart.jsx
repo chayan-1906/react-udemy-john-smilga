1cc5b72c1b787b42bd408c3c80159febb2d6bfb8
Padmanabha Das <padmanabhadas9647@gmail.com>
2023-07-17 20:39:16 +0530

    doughnut chart finished

--- github-search-users/src/components/Charts/ExampleChart.jsx
+++ github-search-users/src/components/Charts/ExampleChart.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react'
+import FusionCharts from 'fusioncharts'
+import Charts from 'fusioncharts/fusioncharts.charts'
+import ReactFC from 'react-fusioncharts'
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion'
 
-const ExampleChart = () => {
-  return <div>chart</div>;
-};
+const ChartComponent = ({data}) => {
+    const chartConfigs = {
+        type: 'bar3d',
+        width: 400,
+        height: 400,
+        dataFormat: 'json',
+        dataSource: {
+            chart: {
+                caption: 'Countries with most oil reserves [2017-18]',
+                subCaption: 'In MMbbl = One million barrels',
+                xAxisName: 'Country',
+                yAxisName: 'Reserves (MMbbl)',
+                numberSuffix: 'K',
+                theme: 'fusion',
+            },
+            data: data,
+        },
+    }
 
-export default ExampleChart;
+    return <ReactFC {...chartConfigs} />
+}
+
+export default ChartComponent
