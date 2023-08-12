8d535a6b6e5ce567521cee2c90c5dd1f3505541e
Padmanabha Das <padmanabhadas9647@gmail.com>
2023-08-12 19:22:54 +0530

    filter functionality for all

--- comfy-sloth/src/context/filter_context.jsx
+++ comfy-sloth/src/context/filter_context.jsx
@@ -49,16 +49,39 @@ export const FilterProvider = ({children}) => {
         dispatch({type: UPDATE_SORT, payload: value})
     }
 
+    const updateFilters = (e) => {
+        let name = e.target.name
+        let value = e.target.value
+        if (name === 'category') {
+            value = e.target.textContent
+        }
+        if (name === 'color') {
+            value = e.target.dataset.color
+        }
+        if (name === 'price') {
+            value = Number(value)
+        }
+        if (name === 'shipping') {
+            value = e.target.checked
+        }
+        dispatch({type: UPDATE_FILTERS, payload: {name, value}})
+    }
+
+    const clearFilters = () => {
+        dispatch({type: CLEAR_FILTERS})
+    }
+
     useEffect(() => {
         dispatch({type: LOAD_PRODUCTS, payload: products})
     }, [products])
 
     useEffect(() => {
+        dispatch({type: FILTER_PRODUCTS})
         dispatch({type: SORT_PRODUCTS})
-    }, [products, state.sort])
+    }, [products, state.sort, state.filters])
 
     return (
-        <FilterContext.Provider value={{...state, setGridView, setListView, updateSort}}>
+        <FilterContext.Provider value={{...state, setGridView, setListView, updateSort, updateFilters, clearFilters}}>
             {children}
         </FilterContext.Provider>
     )
